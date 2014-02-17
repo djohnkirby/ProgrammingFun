555b3a4916d9ec863a23732efa23e6991e363de3
Daniel John Kirby <dkirby@cmu.edu>
2014-02-17 15:04:46 -0500

    Added probs.py

--- primes.c
+++ primes.c
@@ -5,5 +5,15 @@
 /*Generate a list of every prime leq N*/
 int * fetchPrimes( int N )
 {
-
+	/*Generate a list of integers from 2 to N*/
+	int i, j;
+  int * isPrime = malloc( (N + 2)*sizeof(int));
+	isPrime[0] = 0;
+	isPrime[1] = 0;
+	isPrime[2] = 1;
+  for( i = 2; i < N; i ++ )
+	{
+		//Assume all numbers are prime
+		isPrime[i] = 1;
+	}
 }
